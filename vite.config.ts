f71120d5f50c65d109b7f1f7289155c5816c9bb2
agorbatkov <agorbatkov@glowbyteconsulting.com>
2024-11-18 20:44:34 +0300

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const manifest = {
+  theme_color: "#8936FF",
+  background_color: "#2EC6FE",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  dir: "auto",
+  lang: "ru-RU",
+  name: "Gorbatkoff App",
+  short_name: "GAPP",
+  start_url: "/",
+  scope: "/",
+  description: "Наше классное приложение",
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), VitePWA(manifest)],
 });
