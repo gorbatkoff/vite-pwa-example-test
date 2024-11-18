a1bbe39c658d79686d6125b73a636b92c2780182
agorbatkov <agorbatkov@glowbyteconsulting.com>
2024-11-19 00:50:43 +0300

    Add vibration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function triggerVibration() {
+  if ("vibrate" in navigator) {
+    navigator.vibrate(200); // Вибрация длительностью 200 миллисекунд
+    console.log("Телефон вибрирует! 📳");
+  } else {
+    console.log("Вибрация не поддерживается на этом устройстве.");
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,7 +27,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            triggerVibration();
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
@@ -29,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
